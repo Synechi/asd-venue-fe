eaebc250d29582496ccc3992facf8cb09c7e3dcf
Evan Hunter <evan.hunter@student.uts.edu.au>
2019-08-11 17:50:14 +1000

    Add material nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './components/nav/nav.component';
-import { LoginComponent } from './components/login/login.component';
-import { MapComponent } from './components/map/map.component';
+import { MatToolbarModule } from "@angular/material";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NavComponent } from "./components/nav/nav.component";
+import { LoginComponent } from "./components/login/login.component";
+import { MapComponent } from "./components/map/map.component";
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent,
-    LoginComponent,
-    MapComponent
-  ],
+  declarations: [AppComponent, NavComponent, LoginComponent, MapComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    LayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
