b0e7bf953ef64c0bb111839d5c520ff743a6cd8e
Evan Hunter <evan.hunter@student.uts.edu.au>
2019-08-18 16:42:27 +1000

    Changed page title

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -64,7 +64,7 @@ export class MapComponent implements OnInit {
       elementType: "labels",
       stylers: [
         {
-          visibility: "off"
+          visibility: "on"
         }
       ]
     }
