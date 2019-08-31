a03e47dbb7ad82a88b0f15006f934f24b2d3e21c
root <root@DESKTOP-M4FKMLB.localdomain>
2019-08-31 19:02:54 +1000

    Implementing Simple Error Handling

--- src/app/service/friend.service.ts
+++ src/app/service/friend.service.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@angular/core';
 import { User} from '../user';
-import { BehaviorSubject, of } from 'rxjs';
+import { BehaviorSubject, of, throwError } from 'rxjs';
 import { Observable } from 'rxjs'; 
 import { HttpClient, HttpHeaders} from '@angular/common/http';
-
+import { catchError } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class FriendService {
 
   uri = "http://localhost:4000";
-
- 
   
 constructor(private http: HttpClient){
 
@@ -29,8 +27,15 @@ sendFriendRequest(friendID: String): Observable<User[]> {
     })
   };
   
-  return this.http.post<User[]>(`${this.uri}/friendRequest`, {friendID}, httpOptions)
+  return this.http.post<User[]>(`${this.uri}/friendRequest`, {friendID}, httpOptions).pipe(catchError(this.friendExistsError));
 }
 
+friendExistsError(error) {
+
+  let message = "Friend already exists!"
+
+  window.alert(message);
+  return throwError(message);
+}
 
 }
