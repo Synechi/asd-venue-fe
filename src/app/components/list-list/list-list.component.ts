e13c5ed213319bf8cd50869c8e2d3124303e8467
Evan Hunter <evan.hunter@student.uts.edu.au>
2019-09-14 15:44:56 +1000

    List view merge

--- src/app/components/list-list/list-list.component.ts
+++ src/app/components/list-list/list-list.component.ts
@@ -1,28 +1,80 @@
 import { Component, OnInit, NgModule, NgZone } from "@angular/core";
 import { GoogleMapsService } from "../../service/google-maps.service";
+import { MarkerManager } from "@agm/core";
 
 declare var google: any;
 
-
 @Component({
-  selector: 'app-list-list',
-  templateUrl: './list-list.component.html',
-  styleUrls: ['./list-list.component.css']
+  selector: "app-list-list",
+  templateUrl: "./list-list.component.html",
+  styleUrls: ["./list-list.component.css"]
 })
 export class ListListComponent implements OnInit {
-  venues = [
-    new Venue("ChIJDTR29iauEmsR97nGzWimbMo","" , ""),
-    new Venue("ChIJISFoEiKuEmsR8TMqpG8xgwQ", "", ""),
-    new Venue("ChIJC78QMReuEmsR47yBEa6iDPQ", "", ""),
-    new Venue("ChIJ65p_3jyuEmsRuwkbKixObtM", "", ""),
-  ];
   constructor(
     private gMapsService: GoogleMapsService,
     private __zone: NgZone
-  ) { }
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  markers: marker[] = [];
+
+  // Map Center
+  latitude = -33.870752;
+  longitude = 151.208221;
+  // Marker Loader
+  loadMarkers($event) {
+    this.getPlaces($event);
+  }
+
+  showRev = false;
+  selectedOption: string = " ";
+  //Event Handler For The Select Element's Change Event
+  selectChangeHandler(event: any) {
+    //Update The UI
+    this.selectedOption = event.target.value;
+
+    if (this.selectedOption == "Visited") {
+      this.showRev = true;
+    } else {
+      this.showRev = false;
     }
+  }
+
+  getPlaces(map: any) {
+    this.gMapsService.getBarRest(map).subscribe(result => {
+      this.__zone.run(() => {
+        console.log(result);
+
+        for (var place of result) {
+          var labelType = "";
+          for (let i = 0; i < 3; i++) {
+            if (place.types[i] === "bar") {
+              labelType = "Bar";
+            }
+            if (place.types[i] === "restaurant") {
+              labelType = "Restaurant";
+            }
+          }
+          this.markers.push({
+            name: place.name,
+            address: place.vicinity,
+            label: labelType
+          });
+          error => console.log(error);
+        }
+      });
+    });
+  }
+
+  arraysort_name() {
+    this.markers.sort((a, b) => (a.name > b.name ? 1 : -1));
+  }
+  arraysort_location() {
+    this.markers.sort((a, b) => (a.address > b.address ? 1 : -1));
+  }
+  arraysort_genre() {
+    this.markers.sort((a, b) => (a.label > b.label ? 1 : -1));
+  }
 
   getDetail(placeID: string, map: any) {
     this.gMapsService.getDetails(placeID, map).subscribe(result => {
@@ -31,12 +83,20 @@ export class ListListComponent implements OnInit {
       });
     });
   }
-
 }
-export class Venue {
-  constructor(
-      public id: string,
-      public name: string,
-      public address: string
-      ) { }
-  }
+
+export class marker {
+  name: string;
+  address: string;
+  label: string;
+}
+
+//export class Venue {
+//constructor(
+//  public id: string,
+//public name: string,
+//public address: string,
+//public genre: string
+//) { }
+//}
+//test
