c89221617758e88efcd924cc35210a7edfaa813a
Thomas <Thomas.Congerton@student.uts.edu.au>
2019-09-15 17:36:54 +1000

    Testing Files

---
+++ src/app/components/list-list/Testing/Steps 2.js
@@ -0,0 +1,16 @@
+const {Given, When, Then} = require('cucumber')
+
+Given('The user is on the venue list page',function()
+{
+    
+})
+
+When('The column header is clicked',function()
+{
+    
+})
+
+Then('Table will be in alphabetical order based on venue name', function()
+{
+
+})
