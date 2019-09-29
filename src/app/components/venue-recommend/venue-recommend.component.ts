aa61b3b77ae9ec60146e0d9b957886b4b38d0bf9
Thomas <Thomas.Congerton@student.uts.edu.au>
2019-09-29 22:14:25 +1000

    Added actual recommendation

--- src/app/components/venue-recommend/venue-recommend.component.ts
+++ src/app/components/venue-recommend/venue-recommend.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, NgModule, NgZone } from "@angular/core";
 import { GoogleMapsService } from "../../service/google-maps.service";
+import { MarkerManager } from "@agm/core";
 import { UserService } from "../../service/user.service";
 
 @Component({
@@ -17,6 +18,7 @@ export class VenueRecommendComponent implements OnInit {
 
   ngOnInit() {}
   markers: marker[] = [];
+  markers2: marker2[] = [];
 
   // Map Center
   latitude = -33.870752;
@@ -25,72 +27,79 @@ export class VenueRecommendComponent implements OnInit {
   loadMarkers($event) {
     this.getPlaces($event);
   }
-  
-  // Loads Markers once map is loaded
-  getPlaces(map: any) {
-    //Used to generate test data
-    this.gMapsService.getBarRest(map).subscribe(result => {
+
+  getPlaces(map: any)
+  {
+    //========================================================================//
+    this.userService
+    .getListsByID(localStorage.getItem("id"))
+    .subscribe(result => {
       this.__zone.run(() => {
-        for (var place of result) {
-          // this.markers.push({
-          //   lat: place.geometry.location.lat(),
-          //   lng: place.geometry.location.lng(),
-          //   name: place.name,
-          //   address: place.vicinity,
-          //   label: labelType,
-          //   icon: {
-          //     url: place.icon,
-          //     scaledSize: {
-          //       height: 20,
-          //       width: 20
-          //     }
-          //   },
-          //   rating: place.rating,
-          //   open: openHour,
-          //   placeID: place.placeID
-          // });
-          error => console.log(error);
+        // Stringify and then parses the observable object to access the child data.
+        var stringObj = JSON.stringify(result);
+        var list = JSON.parse(stringObj);
+        // Loops over the users venues lists so that the it can access each lists stored venues.
+        for (var key in list.venuelists) {
+          // Loops over the lists venues to generate each marker on the map.
+          for (var test in list.venuelists[key].venues) {
+            // Search the google maps Place API with the Place ID and returns an obserable object with the details of that place.
+            this.gMapsService
+              .getDetails(list.venuelists[key].venues[test].placeID, map)
+              .subscribe(details => {
+                this.__zone.run(() => {
+                  this.markers2.push({
+                    name: details.name,
+                    address: details.formatted_address,
+                    list: list.venuelists[key].name,
+                    label: null,
+                  });
+                });
+              });
+          }
         }
       });
     });
-    //Pulls venues from current users venue lists
-    this.userService
-      .getListsByID(localStorage.getItem("id"))
-      .subscribe(result => {
-        this.__zone.run(() => {
-          // Stringify and then parses the observable object to access the child data.
-          var stringObj = JSON.stringify(result);
-          var list = JSON.parse(stringObj);
-          // Loops over the users venues lists so that the it can access each lists stored venues.
-          for (var key in list.venuelists) {
-            // Loops over the lists venues to generate each marker on the map.
-            for (var test in list.venuelists[key].venues) {
-              // Search the google maps Place API with the Place ID and returns an obserable object with the details of that place.
-              this.gMapsService
-                .getDetails(list.venuelists[key].venues[test].placeID, map)
-                .subscribe(details => {
-                  this.__zone.run(() => {
-                    this.markers.push({
-                      lat: details.geometry.location.lat(),
-                      lng: details.geometry.location.lng(),
-                      name: details.name,
-                      address: details.formatted_address,
-                      list: list.venuelists[key].name,
-                      label: null,
-                    });
-                  });
-                });
-            }
+    //========================================================================//
+  this.gMapsService.getBarRest(map).subscribe(result => {
+    this.__zone.run(() => {
+      console.log(result);
+      for (var place of result) {
+        var labelType = "";
+        for (let i = 0; i < 3; i++) {
+          if (place.types[i] === "bar") {
+            labelType = "bar";
           }
+          if (place.types[i] === "restaurant") {
+            labelType = "restaurant";
+          }
+        }
+        if (place.rating > 4){
+          //if(place.){
+        this.markers.push({
+          name: place.name,
+          address: place.vicinity,
+          label: labelType,
+          rating: place.rating
         });
-      });
-    }
-  }
+     // }
+      }
+        error => console.log(error);
+      }
+    });
+  });
+}
+
+}
+
     export class marker {
-      lat: number;
-      lng: number;
       name: string;
-      list: string;
       address: string;
       label: string;
+      rating: Int16Array;
+    }   
+     export class marker2 {
+      name: string;
+      address: string;
+      label: string;
+      list: string;
     }
