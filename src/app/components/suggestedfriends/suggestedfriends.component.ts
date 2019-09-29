eba38524f48f63354319c123dd3737a53553e51c
root <root@DESKTOP-M4FKMLB.localdomain>
2019-09-29 15:58:55 +1000

    Retrying final changes again

--- src/app/components/suggestedfriends/suggestedfriends.component.ts
+++ src/app/components/suggestedfriends/suggestedfriends.component.ts
@@ -11,7 +11,7 @@ import { User } from "../../user";
 })
 export class SuggestedfriendsComponent implements OnInit {
   suggestedFriends: User[] = [];
-  status: boolean;
+  searchBox: string;
   statusSuccess: boolean;
   statusSuccessMessage: string;
   placeholderStatus: boolean;
