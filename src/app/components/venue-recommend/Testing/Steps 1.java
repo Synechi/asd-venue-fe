2c13c43f334c047314bd635da38d7d88f314f6cc
C0NGY <49135122+C0NGY@users.noreply.github.com>
2019-10-13 21:06:07 +1100

    Testing Added

---
+++ src/app/components/venue-recommend/Testing/Steps 1.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+
+public class RecFeature{
+
+WebDriver driver;
+
+@Given("I click on the Recommend Venues button")
+public void ClickButton() throws Throwable {
+    
+}
+
+@When("The button is clicked")
+public void ButtonAction() throws Throwable {
+    
+}
+
+@Then("The webpage should change to the Venue list page")
+public void Newpage() throws Throwable {
+    
+}
+}
